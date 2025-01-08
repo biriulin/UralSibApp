ebee266cb5e8475aa4c71a11f655e778918187da
Birulin Artemii <biriulinartemii@gmail.com>
2025-01-08 06:33:59 +0300

    fix: deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/UralSibApp',
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
     webpack: (config) => {
         config.module.rules.push({
             test: /\.svg$/i,
